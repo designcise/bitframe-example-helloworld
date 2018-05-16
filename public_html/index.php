bb13db0de921ac8e17f488a8ff9c85970ba63993
Designcise <designcise@gmail.com>
2018-05-16 14:19:45 +0500

    Add files via upload

--- public_html/index.php
+++ public_html/index.php
@@ -12,12 +12,12 @@ $app = new \BitFrame\Application();
 $app->addMiddleware([
     // response emitter
     // @see https://www.bitframephp.com/middleware/response-emitter
-	\BitFrame\Message\DiactorosResponseEmitter::class,
-	
+    \BitFrame\Message\DiactorosResponseEmitter::class,
+    
     // custom middleware to output (string) "Hello World!"
     // @see https://www.bitframephp.com/doc/application/add-middleware
-	new class implements MiddlewareInterface {
-		public function process(Request $request, Handler $handler): Response 
+    new class implements MiddlewareInterface {
+        public function process(Request $request, Handler $handler): Response 
         {
             $response = $handler->handle($request);
             $response->getBody()->write("Hello World!");
